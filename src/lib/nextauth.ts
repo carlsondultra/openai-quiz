6320ecc09297c7d96cce7a8376d784f0de566b0e
carlsondultra <carlsondultra@users.noreply.github.com>
2023-08-12 21:19:24 -0400

    installing prisma adapter for next auth

--- src/lib/nextauth.ts
+++ src/lib/nextauth.ts
@@ -1,9 +1,11 @@
 import { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "./db"
 
 export const authOptions: NextAuthOptions = {
     session : {
         strategy: 'jwt'
     },
     secret: process.env.NEXTAUTH_SECRET,
-    
+    adapter: PrismaAdapter(prisma),
 }
