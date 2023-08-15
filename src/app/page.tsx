00d9502549ab9aab5491082ffb038f8cff94f14b
carlsondultra <carlsondultra@users.noreply.github.com>
2023-08-15 16:17:32 -0400

    adding to homepage design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
-export default function Home() {
+import SignInButton from "@/components/SignInButton";
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardDescription, CardTitle, CardContent } from "@/components/ui/card";
+import {prisma} from '@/lib/db'
+import { getAuthSession } from "@/lib/nextauth";
+
+export default async function Home() {
+  const session = await getAuthSession()
   return (
-   <h1 className="text-red-600"> Hello World </h1>
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <Card className="w-[300px]">
+        <CardHeader>
+          <CardTitle>Welcome to my application!</CardTitle>
+          <CardDescription>This is description text</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <SignInButton text="Sign In with Google!"/>
+        </CardContent>
+      </Card>
+    </div>
   )
 }
