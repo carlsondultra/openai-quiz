36186f562638656d6bec6dc7dc635f2df533199e
carlsondultra <carlsondultra@users.noreply.github.com>
2023-08-14 20:59:15 -0400

    added button to sign out on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import { getAuthSession } from '@/lib/nextauth'
 import Link from "next/link";
 import React from 'react'
 import SignInButton from './SignInButton';
+import UserAccountNav from './UserAccountNav';
 
 type Props = {}
 
@@ -19,7 +20,7 @@ const Navbar = async (props: Props) => {
 
                 <div className="flex items-center">
                     {session?.user ? (
-                        <h1>Hello {session.user.name}</h1>
+                        <UserAccountNav user={session.user} />
                     ) : (
                         <SignInButton text="Sign In" />
                     )}
