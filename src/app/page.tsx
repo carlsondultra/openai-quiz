588e0551529e470b0ef20f6d215432897b90668f
carlsondultra <carlsondultra@users.noreply.github.com>
2023-08-16 20:19:33 -0400

    creating dashboard when logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import SignInButton from "@/components/SignInButton";
-import { Button } from "@/components/ui/button";
 import { Card, CardHeader, CardDescription, CardTitle, CardContent } from "@/components/ui/card";
 import {prisma} from '@/lib/db'
 import { getAuthSession } from "@/lib/nextauth";
+import { redirect } from 'next/navigation'
 
 export default async function Home() {
   const session = await getAuthSession()
+  if(session?.user) {
+    //user is logged in
+    return redirect('/dashboard')
+  }
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <Card className="w-[300px]">
