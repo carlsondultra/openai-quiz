d71d119edded819890fef9a4b31ab75d60880536
carlsondultra <carlsondultra@users.noreply.github.com>
2023-08-18 15:41:08 -0400

    initializing quiz form using zod

--- src/components/QuizCreation.tsx
+++ src/components/QuizCreation.tsx
@@ -1,9 +1,43 @@
+'use client'
 import React from 'react'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card'
+import {useForm} from 'react-hook-form'
+import { quizCreationSchema } from '@/schemas/form/quiz'
+import { z } from 'zod'
+import {zodResolver} from '@hookform/resolvers/zod'
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
+
 
 type Props = {}
 
+//quiz creation schema is a zod object, then using zod to infer
+type Input = z.infer<typeof quizCreationSchema>
+
 const QuizCreation = (props: Props) => {
+    const form = useForm<Input>({
+        resolver: zodResolver(quizCreationSchema),
+        defaultValues: {
+            amount: 3,
+            topic: "",
+            type: "open_ended"
+        }
+    })
+
+function onSubmit (input: Input){
+    alert(JSON.stringify(input,null,2))
+}
+
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <Card>
@@ -12,7 +46,27 @@ const QuizCreation = (props: Props) => {
                 <CardDescription>Choose a topic!</CardDescription>
             </CardHeader>
             <CardContent>
-                
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                        <FormField
+                        control={form.control}
+                        name="topic"
+                        render={({ field }) => (
+                            <FormItem>
+                            <FormLabel>Topic</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Enter a topic..." {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                Please provide a topic
+                            </FormDescription>
+                            <FormMessage />
+                            </FormItem>
+                        )}
+                        />
+                        <Button type="submit">Submit</Button>
+                    </form>
+                </Form>
             </CardContent>
         </Card>
     </div>
