5eaebd7aed874495aa330bf304cc921eee5af722
carlsondultra <carlsondultra@users.noreply.github.com>
2023-08-18 16:55:30 -0400

    finishing up the input form

--- src/components/QuizCreation.tsx
+++ src/components/QuizCreation.tsx
@@ -16,6 +16,8 @@ import {
 } from "@/components/ui/form"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import { BookOpen, CopyCheck } from "../../node_modules/lucide-react"
+import { Separator } from './ui/separator'
 
 
 
@@ -37,6 +39,8 @@ const QuizCreation = (props: Props) => {
 function onSubmit (input: Input){
     alert(JSON.stringify(input,null,2))
 }
+
+form.watch(); //re-renders component when state is changed
 
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
@@ -85,6 +89,29 @@ function onSubmit (input: Input){
                             </FormItem>
                         )}
                         />
+                        <div className="flex justify-between">
+                            <Button 
+                                type='button'
+                                onClick={() => {
+                                    form.setValue('type', 'mcq')
+                                }}
+                                className="w-1/2 rounded-none rounded-l-lg"
+                                variant={form.getValues('type') === 'mcq' ? 'default' : 'secondary'}
+                            >
+                                <CopyCheck className="w-4 h-4 mr-2"/>Multiple Choice
+                            </Button>
+                            <Separator orientation="vertical"/>
+                            <Button 
+                                type='button'
+                                onClick={() => {
+                                    form.setValue('type', 'open_ended')
+                                }}
+                                className="w-1/2 rounded-none rounded-r-lg"
+                                variant={form.getValues('type') === 'open_ended' ? 'default' : 'secondary'}
+                            >
+                                <BookOpen className="w-4 h-4 mr-2" />Open Ended
+                            </Button>
+                        </div>
                         <Button type="submit">Submit</Button>
                     </form>
                 </Form>
