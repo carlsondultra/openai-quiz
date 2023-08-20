47aa02670b604ce6ad0c7c454edf21f742c72e9d
carlsondultra <carlsondultra@users.noreply.github.com>
2023-08-20 17:59:31 -0400

    hiding answer and only showing question and options

--- src/app/play/mcq/[gameId]/page.tsx
+++ src/app/play/mcq/[gameId]/page.tsx
@@ -19,8 +19,14 @@ const MCQPage = async ({params: {gameId} }: Props) => {
             id: gameId
         },
         include: {
-            questions: true
-        }
+            questions: {
+                select: {
+                    id: true,
+                    question: true,
+                    options: true,
+                },
+            },
+        },
     })
   return (
     <pre>{JSON.stringify(game, null, 2)}</pre>
