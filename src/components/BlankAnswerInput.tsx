a7818db950407a912557da8b4f0c459ef7c65db4
carlsondultra <carlsondultra@users.noreply.github.com>
2023-08-24 15:53:10 -0400

    retrieving key words from answer

--- src/components/BlankAnswerInput.tsx
+++ src/components/BlankAnswerInput.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import keyword_extractor from 'keyword-extractor'
 
 type Props = {
     answer: string
 }
 
 const BlankAnswerInput = ({answer}: Props) => {
+  //dont need to re-calculate every time
+  const keywords = React.useMemo(() => {
+      const words = keyword_extractor.extract(answer, {
+        language: "english",
+        remove_digits: true,
+        return_changed_case: false,
+        remove_duplicates: false,
+      })
+  }, [answer])
   return (
     <div className="flex justify-start w-full mt-4">
         <h1 className="text-xl font-semibold">{answer}</h1>
