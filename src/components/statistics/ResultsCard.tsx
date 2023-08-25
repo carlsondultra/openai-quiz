56992172f0dd8dc3bb7c020bdfadb1716b7ad564
carlsondultra <carlsondultra@users.noreply.github.com>
2023-08-25 18:03:47 -0400

    adding ternary operator depending on accuracy percentage

--- src/components/statistics/ResultsCard.tsx
+++ src/components/statistics/ResultsCard.tsx
@@ -14,13 +14,34 @@ const ResultsCard = ({accuracy}: Props) => {
             <Award />
         </CardHeader>
         <CardContent className="flex flex-col items-center justify-center h-3/5">
-            {accuracy > 75 && (
+            {accuracy > 75 ? (
                 <>
                 <Trophy className="mr-4" stroke='gold' size={50} />
                 <div className="flex flex-col text-2xl font-semibold text-yellow-400">
                     <span>Amazing!</span>
                     <span className="text-sm text-center text-black opacity-50">
                         {'> 75% accuracy'}
+                    </span>
+                </div>
+                </>
+            ) :
+            accuracy > 25 ? (
+                <>
+                <Trophy className="mr-4" stroke='silver' size={50} />
+                <div className="flex flex-col text-2xl font-semibold text-yellow-400">
+                    <span>Nice!</span>
+                    <span className="text-sm text-center text-black opacity-50">
+                        {'> 25% accuracy'}
+                    </span>
+                </div>
+                </>
+            ) : (
+                <>
+                <Trophy className="mr-4" stroke='gold' size={50} />
+                <div className="flex flex-col text-2xl font-semibold text-yellow-400">
+                    <span>Nice Try!</span>
+                    <span className="text-sm text-center text-black opacity-50">
+                        {'> % accuracy'}
                     </span>
                 </div>
                 </>
