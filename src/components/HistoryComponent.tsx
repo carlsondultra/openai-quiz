e69c9ef69f2b4ad10deb85cf81830017935f7ae6
carlsondultra <carlsondultra@users.noreply.github.com>
2023-08-26 10:45:34 -0400

    fixing spacing layout on history component

--- src/components/HistoryComponent.tsx
+++ src/components/HistoryComponent.tsx
@@ -29,20 +29,21 @@ const HistoryComponent = async ({limit, userId}: Props) => {
               ) : (
                 <Edit2 className="mr-3" />
               )}
-              <div className="ml-4 space-y-1"></div>
-              <Link 
-                href={`/statistics/${game.id}`} 
-                className="text-base font-medium leading-none underline"
-              >
-                {game.topic}
-              </Link>
-              <p className="flex items-center px-2 py-1 text-xs text-white rounded-lg w-fit bg-slate-800">
-                <Clock className="w-4 h-4 mr-1" />
-                {new Date(game.timeStarted).toLocaleDateString()}
-              </p>
-              <p className="text-sm text-muted-foreground">
-                {game.gameType === "mcq" ? "MCQ" : "Open Ended"}
-              </p>
+              <div className="ml-4 space-y-1">
+                  <Link 
+                    href={`/statistics/${game.id}`} 
+                    className="text-base font-medium leading-none underline"
+                  >
+                    {game.topic}
+                  </Link>
+                <p className="flex items-center px-2 py-1 text-xs text-white rounded-lg w-fit bg-slate-800">
+                  <Clock className="w-4 h-4 mr-1" />
+                  {new Date(game.timeStarted).toLocaleDateString()}
+                </p>
+                <p className="text-sm text-muted-foreground">
+                  {game.gameType === "mcq" ? "MCQ" : "Open Ended"}
+                </p>
+              </div>
             </div>
           </div>
         )
