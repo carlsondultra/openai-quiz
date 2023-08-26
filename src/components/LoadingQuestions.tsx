6fbbc4c052e65749249813ffe815b46574563d7a
carlsondultra <carlsondultra@users.noreply.github.com>
2023-08-26 09:53:27 -0400

    adding loading bar and text to loading screen

--- src/components/LoadingQuestions.tsx
+++ src/components/LoadingQuestions.tsx
@@ -1,9 +1,45 @@
+'use client'
 import Image from 'next/image'
 import React from 'react'
+import { Progress } from './ui/progress'
 
 type Props = {}
 
+const loadingTexts = [
+    "Gathering a Galaxy of Questions...",
+    "Warming up the Engines...",
+    "Summoning Trivia Titans...",
+    "Plotting a Course to Alderaan...",
+    "Initializing the Quest for Knowledge...",
+]
+
 const LoadingQuestions = (props: Props) => {
+    const [progress, setProgress] = React.useState(0)
+    const [loadingText, setLoadingText] = React.useState(loadingTexts[0])
+    React.useEffect(() =>{
+        const interval = setInterval(() =>{
+            const randomIndex = Math.floor(Math.random() * loadingTexts.length)
+            setLoadingText(loadingTexts[randomIndex])
+        }, 2000)
+        return () => clearInterval(interval)
+    })
+
+    React.useEffect(() => {
+        const interval = setInterval(() => {
+            setProgress(prev => {
+                if(prev === 100){
+                    return 0
+                }
+                if (Math.random() < 0.1){
+                    return prev + 2
+                }
+                return prev + 0.5
+            })
+        }, 100)
+        return () => clearInterval(interval);
+    })
+
+
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[70vw] md:w-[60vw] flex flex-col items-center">
         <Image
@@ -12,6 +48,8 @@ const LoadingQuestions = (props: Props) => {
             height={400}
             alt="loading animation"
         />
+        <Progress value={progress} className="w-full mt-4"/>
+        <h1 className="mt-2 text-xl">{loadingText}</h1>
     </div>
   )
 }
