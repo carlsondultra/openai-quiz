f565f32ee01b4d0ec820525becefd795b627419c
carlsondultra <carlsondultra@users.noreply.github.com>
2023-08-29 14:05:44 -0400

    editing landing page text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,11 +14,14 @@ export default async function Home() {
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <Card className="w-[300px]">
         <CardHeader>
-          <CardTitle>Welcome to my application!</CardTitle>
-          <CardDescription>This is description text</CardDescription>
+          <CardTitle>Introducing "Quiz-It": Your Ultimate AI-Powered Quiz Creation Platform</CardTitle>
+          <CardDescription>
+            Welcome to Quiz-It, 
+            the cutting-edge web application that harnesses the power of artificial intelligence to revolutionize the way quizzes are generated.
+          </CardDescription>
         </CardHeader>
         <CardContent>
-          <SignInButton text="Sign In with Google!"/>
+          <SignInButton text="Sign In with Google to get started!"/>
         </CardContent>
       </Card>
     </div>
