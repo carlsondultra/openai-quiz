a821845963a13c119c526bd61526c3d7622199b3
carlsondultra <carlsondultra@users.noreply.github.com>
2023-08-29 14:15:41 -0400

    fixing darkmode for user avatar name

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -22,7 +22,7 @@ const UserAccountNav = ({user}: Props) => {
         <DropdownMenuContent className="bg-white" align='end'>
             <div className="flex items-center justify-start gap-2 p-2">
                 <div className="flex flex-col space-y-1 leading-none">
-                    {user.name && <p className="font-medium">{user.name}</p>}
+                    {user.name && <p className="font-medium dark:text-zinc-700">{user.name}</p>}
                     {
                         user.email && (
                             <p className="w-[200px] truncate text-sm text-zinc-700">
@@ -32,10 +32,11 @@ const UserAccountNav = ({user}: Props) => {
                     }
                 </div>
             </div>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem asChild>
+
+            {/* <DropdownMenuSeparator /> */}
+            {/* <DropdownMenuItem asChild>
                     <Link href='/'> This is text </Link>
-            </DropdownMenuItem>
+            </DropdownMenuItem> */}
 
             <DropdownMenuSeparator />
             <DropdownMenuItem
